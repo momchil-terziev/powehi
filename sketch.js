310f8eba732ae275d1f59f8d928074a602726fdc
momchil-terziev <97561226+momchil-terziev@users.noreply.github.com>
2023-05-08 16:53:09 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
-// var c = 30;
-// var G = 3.54;
- const dt = 2;
+ const c = 30;
+ const G = 3.54;
+ const dt = 0.1;
 
 let m87;
 
@@ -10,14 +10,7 @@ let start, end;
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  button1 = createButton("Set G to");
-  button1.mousePressed(resetSketch);
-  Gvalue = createInput(3.54);
-  
-  button2 = createButton("Set c to");
-  button2.mousePressed(resetSketch);
-  Cvalue = createInput(30);
-
+ 
   m87 = new Blackhole(width / 2, height / 2, 10000);
   rcol1 = random(226);
   rcol2 = random(226);
@@ -31,18 +24,6 @@ function setup() {
   }
 }
 
-function resetSketch() {
-  rcol1 = random(226);
-  rcol2 = random(226);
-  rcol3 = random(226);
-  G = Gvalue.value();
-  c = Cvalue.value();
-  // m87 = new Blackhole(width / 2, height / 2, 10000);
- // dt = 2;
-  for (let y = 0; y < start; y += 10) {
-    particles.push(new Photon(width - 20, y));
-  }
-}
 
 function draw() {
   background(255);
